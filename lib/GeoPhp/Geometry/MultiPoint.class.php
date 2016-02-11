4877b8c3d998dc7a46f1c48a116f060de31d2bc8
Gert Holterman <gert@peridot.nl>
2016-02-11 17:40:09 +0100

    re arranged code from hayes and namespaced it
    good for compatibility

---
+++ lib/GeoPhp/Geometry/MultiPoint.class.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GeoPhp\Geometry;
+
+/**
+ * MultiPoint: A collection Points  
+ */
+class MultiPoint extends Collection
+{
+  protected $geom_type = 'MultiPoint';
+  
+  public function numPoints() {
+    return $this->numGeometries();
+  }
+  
+  public function isSimple() {
+    return TRUE;
+  }
+  
+  // Not valid for this geometry type
+  // --------------------------------
+  public function explode() { return NULL; }
+}
+
---
+++ lib/GeoPhp/Geometry/MultiPolygon.class.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace GeoPhp\Geometry;
+
+/**
+ * 
+ * MultiPolygon: A collection of Polygons
+ */
+class MultiPolygon extends Collection 
+{
+  protected $geom_type = 'MultiPolygon';
+}
